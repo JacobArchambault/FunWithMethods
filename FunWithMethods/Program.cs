d4b54136077346326c1602f389a79e378a02097d
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-15 14:42:54 -0400

    Change Add() to use output parameter.

--- FunWithMethods/Program.cs
+++ FunWithMethods/Program.cs
@@ -12,20 +12,15 @@ namespace FunWithMethods
         {
             Console.WriteLine("***** Fun with Methods *****/n");
 
-            // Pass two variables in by value.
-            int x = 9, y = 10;
-            Console.WriteLine("Before call: X: {0}, Y: {1}", x, y);
-            Console.WriteLine("Answer is {0}", Add(x, y));
-            Console.WriteLine("After call: X: {0}, Y: {1}", x, y);
+            //No need to assign initial value to local variables used as output parameters, provided the first time you use them is as output arguments.
+            // C# 7 allows for out parameters to be declared in the method call.
+            Add(90, 90, out int ans);
+            Console.WriteLine("90 + 90 = {0}", ans);
             Console.ReadLine();
         }
-        static int Add(int x, int y)
+        static void Add(int x, int y, out int ans)
         {
-            int ans = x + y;
-            // Caller will not see these changes as you are modifying a copy of the original data
-            x = 10000;
-            y = 88888;
-            return ans;
+            ans = x + y;
         }
     }
 }
